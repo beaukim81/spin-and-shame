bd5dda0788383b1cb36f0c1e17b44f77cdad2243
Kim <mailz4kim@gmail.com>
2026-05-18 20:22:58 +0200

    punt chaos mode

--- src/app/page.js
+++ src/app/page.js
@@ -1427,7 +1427,7 @@ export default function Home() {
       <div className="relative z-10 w-full max-w-[900px] xl:max-w-[1200px] px-4 mx-auto">
 
         <div className="relative backdrop-blur-xl bg-white/5 border border-white/10 rounded-[36px] p-[clamp(20px,4vw,40px)] min-h-[65vh] xl:min-h-[75vh] flex flex-col justify-between">
-          {showPointAnimation && !chaosMode && (
+          {showPointAnimation && (
 
             <motion.div
               initial={{
