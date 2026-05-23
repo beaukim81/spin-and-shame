ac1baee7dba161caf96241bc012f398f98e58c8a
Kim <mailz4kim@gmail.com>
2026-05-23 10:52:57 +0200

    fix

--- src/app/components/StartMenu.jsx
+++ src/app/components/StartMenu.jsx
@@ -323,7 +323,7 @@ export default function StartMenu({
 
                                 spinLetter();
 
-                            }, 150);
+                            }, 500);
 
                             return;
 
